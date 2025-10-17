aca1f066a73d3469a4097196748f9b851ae05324
qqh4 <daniil-vl@mail.ru>
2025-10-17 15:26:10 +0300

    replace dummy build with real sdist packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_first_ci_cd_project",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "Flask>=3.1.2"
+    ],
+    description="Simple demo project for CI/CD pipeline with SAST",
+    author="Your Name",
+    python_requires=">=3.8",
+)
